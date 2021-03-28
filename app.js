0c5f569800e20eb33c823f344d18e3300c8a4c3e
Llewellyn Dsouza <lledsouza2209@gmail.com>
2021-03-28 23:27:12 +0530

    token auth tested, login route added

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ mongoose
 
 const notesRouter = require("./controllers/notes");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const middleware = require("./utils/middleware");
 
 app.use(cors());
@@ -32,6 +33,7 @@ app.use(express.static("build"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use("/api/login", loginRouter);
 app.use("/api/notes", notesRouter);
 app.use("/api/users", usersRouter);
 
